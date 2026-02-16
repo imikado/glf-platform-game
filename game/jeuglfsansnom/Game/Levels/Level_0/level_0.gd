6e986e8f29d4d8fa547c2d69a158221c4d029424
imikado <imikado@gmail.com>
2026-02-16 22:21:15 +0100

    improve mushroom diing

--- game/jeuglfsansnom/Game/Levels/Level_0/level_0.gd
+++ game/jeuglfsansnom/Game/Levels/Level_0/level_0.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 @onready var player:PlatformPlayer=$PlatformPlayer
 
-const DEBUG=true
+const DEBUG=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
